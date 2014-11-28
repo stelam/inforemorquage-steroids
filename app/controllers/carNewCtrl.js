d35a2f78d970213e899ea69dcd18e30ed8fe8862
unknown <ulams99@VM974233.ile.mtl.qc.ca>
2014-11-28 13:38:55 -0500

    jsdoc pour carnewctrl

--- app/controllers/carNewCtrl.js
+++ app/controllers/carNewCtrl.js
@@ -1,62 +1,124 @@
 /**
-* @class angular_module.carApp.ShowCtrl
+* @class angular_module.carApp.NewCtrl
 * @classdesc Contrôleur pour la vue de détails d'une voiture
 */
-carApp.controller('NewCtrl', ['UIInitializer', '$scope', 'CarModel', '$filter', 'ViewManager', '$cordovaDialogs', 'Helpers', '$cordovaToast', 'CameraManager', function (UIInitializer, $scope, CarModel, $filter, ViewManager, $cordovaDialogs, Helpers, $cordovaToast, CameraManager) {
-  // empty the car ojbect
-  $scope.car = CarModel.defaultCar();
-
-  this.messageReceived = function(event) {
+carApp.controller('NewCtrl', ['
+  UIInitializer', 
+  '$scope', 
+  'CarModel', 
+  '$filter', 
+  'ViewManager', 
+  '$cordovaDialogs', 
+  'Helpers', 
+  '$cordovaToast', 
+  'CameraManager', 
+  function (UIInitializer, $scope, CarModel, $filter, ViewManager, $cordovaDialogs, Helpers, $cordovaToast, CameraManager) {
     
-    if (event.data.action == "openFromDrawer" && event.data.viewId == "newCar"){
-      // Native navigation
-      UIInitializer.initNavigationBar('Nouveau véhicule');
-      UIInitializer.initNavigationMenuButton();
-    }
-  }
-  window.addEventListener("message", this.messageReceived);
-
-
-  $scope.requestCreate = function(){
-    CarModel.create($scope.car, $scope.onCarCreateSuccess);
-  };
-
-  $scope.requestCamera = function(){
-    CameraManager.takePicture($scope.imageReceived);
-    return false;
-  }
-
-  $scope.requestFileBrowser = function(){
-    CameraManager.browsePicture($scope.imageReceived); 
-  }
-
-  $scope.imageReceived = function(imageURL){
-    $scope.car.imageURL = imageURL;
-    $scope.$apply();
-  }
-
-
-  $scope.onCarCreateSuccess = function(car){
-    Helpers.cordovaCallbackFix("Création");
+    /* Réinitialiser le modèle voiture */
     $scope.car = CarModel.defaultCar();
 
-    steroids.layers.pop(); 
+   /**
+    * @name this.messageReceived
+    * @function
+    * @memberOf angular_module.carApp.IndexCtrl
+    * @description Méthode appelée en callback lorsqu'un window.postMessage est reçu
+    * @param {Object} event
+    */
+    this.messageReceived = function(event) {
+      
+      /* Si la vue est appelée à partir du menu latéral, on doit faire afficher ces éléments UI natifs manuellement */
+      if (event.data.action == "openFromDrawer" && event.data.viewId == "newCar"){
+        UIInitializer.initNavigationBar('Nouveau véhicule');
+        UIInitializer.initNavigationMenuButton();
+      }
+    }
 
-    window.postMessage({
-      action: "refreshCarById",
-      carId : car.id
-    });
-
-    $cordovaToast.showShortTop('Véhicule créé');
-
-    $scope.form.$setPristine();
-  };
+    /* Écoute des messages (window.postMessage) */
+    window.addEventListener("message", this.messageReceived);
 
 
-  $scope.cancel = function(id) {
-    steroids.layers.pop(); 
-  };
+    /**
+    * @name $scope.requestCreate
+    * @function
+    * @memberOf angular_module.carApp.NewCtrl
+    * @description Demande de crer un nouveau véhicule
+    */  
+    $scope.requestCreate = function(){
+      CarModel.create($scope.car, $scope.onCarCreateSuccess);
+    };
 
-  
-}]);
+    /**
+    * @name $scope.requestCamera
+    * @function
+    * @memberOf angular_module.carApp.NewCtrl
+    * @description Demande de démarrer la caméra de l'appareil
+    */  
+    $scope.requestCamera = function(){
+      CameraManager.takePicture($scope.imageReceived);
+      return false;
+    }
 
+    /**
+    * @name $scope.requestFileBrowser
+    * @function
+    * @memberOf angular_module.carApp.NewCtrl
+    * @description Demande de démarrer l'explorateur de fichiers de l'appareil
+    */  
+    $scope.requestFileBrowser = function(){
+      CameraManager.browsePicture($scope.imageReceived); 
+    }
+
+
+    /**
+    * @name $scope.imageReceived
+    * @function
+    * @memberOf angular_module.carApp.NewCtrl
+    * @description Callback appelé lorsque l'utilisateur à sélectionné une image
+    * @param {string} imageURL - emplacement de l'image sélectionnée
+    */  
+    $scope.imageReceived = function(imageURL){
+      $scope.car.imageURL = imageURL;
+      $scope.$apply();
+    }
+
+
+    /**
+    * @name $scope.onCarCreateSuccess
+    * @function
+    * @memberOf angular_module.carApp.NewCtrl
+    * @description Callback appelé lorsque la voiture a été créé avec succès
+    * @param {Object} car - Objet représentant le véhicule créé
+    */  
+    $scope.onCarCreateSuccess = function(car){
+      /* Une fois le véhicule créé, on réinialise le modèle véhicule pour la création éventuelle d'une autre voiture */
+      $scope.car = CarModel.defaultCar();
+
+      /* Retour à l'index des véhicules */
+      steroids.layers.pop(); 
+
+      /* Rafraîchissement du véhicule nouvellement créé */
+      window.postMessage({
+        action: "refreshCarById",
+        carId : car.id
+      });
+
+      $cordovaToast.showShortTop('Véhicule créé');
+
+      /* Réinitialisation du formulaire */
+      $scope.form.$setPristine();
+    };
+
+
+    /**
+    * @name $scope.cancel
+    * @function
+    * @memberOf angular_module.carApp.NewCtrl
+    * @description L'utilisateur a annulé la création d'un véhicule
+    */  
+    $scope.cancel = function(id) {
+      steroids.layers.pop(); 
+    };
+
+
+  }
+]);
